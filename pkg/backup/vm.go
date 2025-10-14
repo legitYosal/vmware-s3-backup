39fb676ba80f8d4a3a73864a01a598b7fe417327
Yosef S <flaslnm@gmail.com>
2025-10-14 19:54:02 +0330

    add verbose log

--- pkg/backup/vm.go
+++ pkg/backup/vm.go
@@ -154,8 +154,10 @@ func (c *DetailedVirtualMachine) StartNBDSockets(ctx context.Context, vmKey stri
 		case *types.VirtualDisk:
 			backing := disk.Backing.(types.BaseVirtualDeviceFileBackingInfo)
 			info := backing.GetVirtualDeviceFileBackingInfo()
-			slog.Debug("Starting NBD socket for disk", "diskName", disk.DeviceInfo.GetDescription().Label)
 			password := c.S3BackupClient.Configuration.VMWarePassword
+
+			slog.Debug("Starting NBD socket for disk", "diskName", disk.DeviceInfo.GetDescription().Label)
+
 			socket, err := nbdkit.NewNBDKitSocketConfig(
 				c.S3BackupClient.VDDKConfig.Endpoint.Host,
 				c.S3BackupClient.VDDKConfig.Endpoint.User.Username(),
