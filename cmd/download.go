f543f0a5bd1dd319b8de969eebff5c32f6a14dc2
Yosef S <flaslnm@gmail.com>
2025-10-19 17:58:31 +0330

    this time i am sure args issue is fixed in download command

--- cmd/download.go
+++ cmd/download.go
@@ -1,17 +1,35 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/spf13/cobra"
+)
 
-var downloadCmd = &cobra.Command{
-	Use:   "download",
-	Short: "Download a VM from S3",
-	Long:  `The download command downloads a VM from S3 to a local directory.`,
-	// Run: func(cmd *cobra.Command, args []string) {
-	// 	// By not providing a Run function, this command will show its help text
-	// 	// if called without a subcommand.
-	// },
+var downloadBackupCmd = &cobra.Command{
+	Use:   "download-backup <VM_KEY> <DISK_KEY> <LOCAL_PATH>",
+	Short: "Download a backup from S3",
+	Long:  `The download-backup command downloads a backup from S3 to a local directory.`,
+	Args:  cobra.ExactArgs(3),
+	RunE: func(cmd *cobra.Command, args []string) error {
+		vmKey := args[0]
+		diskKey := args[1]
+		localPath := args[2]
+		vmList, err := cli.S3DB.ListVirtualObjectMachines(cmd.Context(), vmKey)
+		if err != nil {
+			return err
+		}
+		vm := vmList[0]
+		for _, disk := range vm.Disks {
+			if disk.DiskKey == diskKey {
+				err = disk.RestoreDiskToLocalPath(cmd.Context(), cli.S3DB, localPath)
+				if err != nil {
+					return err
+				}
+			}
+		}
+		return nil
+	},
 }
 
 func init() {
-	rootCmd.AddCommand(downloadCmd)
+	rootCmd.AddCommand(downloadBackupCmd)
 }
