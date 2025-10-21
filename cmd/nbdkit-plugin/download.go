be9165d80797a4fbbf86589a0477973aeb16099a
Yosef S <flaslnm@gmail.com>
2025-10-21 20:32:40 +0330

    fully sync download

--- cmd/nbdkit-plugin/download.go
+++ cmd/nbdkit-plugin/download.go
@@ -11,48 +11,30 @@ import (
 
 type SafeDownload struct {
 	mutex sync.Mutex
-	locks map[int32]*sync.Mutex
 }
 
 func NewSafeDownload() *SafeDownload {
-	return &SafeDownload{
-		locks: make(map[int32]*sync.Mutex),
-	}
+	return &SafeDownload{}
 }
 
 func (s *SafeDownload) LoadPartFromS3(partNumber int32) error {
 	partKey := vms3.GetS3FullObjectKey(diskManifest.ObjectKey, partNumber+1)
-	buff, err := s3DB.GetObject(context.Background(), partKey)
+	var err error
+	buffer, err := s3DB.GetObject(context.Background(), partKey)
 	if err != nil {
 		return fmt.Errorf("failed to get object: %w", err)
 	}
-	buff, err = vms3.DecompressBufferZstd(buff, diskManifest.FullChunksMetadata[partNumber].Length)
+	buffer, err = vms3.DecompressBufferZstd(buffer, diskManifest.FullChunksMetadata[partNumber].Length)
 	if err != nil {
 		return fmt.Errorf("failed to decompress data: %w", err)
 	}
-	lruCache.AddPart(partNumber, buff)
+	lruCache.AddPart(partNumber, buffer)
 	return nil
 }
 
 func (s *SafeDownload) LoadPart(partNumber int32) error {
 	s.mutex.Lock()
-	partLock, ok := s.locks[partNumber]
-	if !ok {
-		partLock = &sync.Mutex{}
-		s.locks[partNumber] = partLock
-	}
-	s.mutex.Unlock()
-	s.locks[partNumber].Lock()
-
-	defer func() {
-		lock, ok := s.locks[partNumber]
-		if ok {
-			lock.Unlock()
-			s.mutex.Lock()
-			delete(s.locks, partNumber)
-			s.mutex.Unlock()
-		}
-	}()
+	defer s.mutex.Unlock()
 
 	if diskManifest.FullChunksMetadata[partNumber].Compression == vms3.S3CompressionZstd {
 		if lruCache.HasPart(partNumber) {
